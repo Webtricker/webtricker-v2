b21c71876c57edc7cfd0953b02070e69b6a8b999
dev-ahad-ali <ahadali.dev@gmail.com>
2025-08-18 21:44:17 +0600

    Update: dynamic metadata added for services page

--- src/app/(public pages)/services/page.tsx
+++ src/app/(public pages)/services/page.tsx
@@ -4,9 +4,35 @@ import OurServices from "./components/OurServices";
 import ServicesPanelWrapper from "./components/ServicesPanelWrapper";
 import ServicePanels from "./components/ServicePanels";
 import { getServicesData } from "@/utils/pageData";
+import shortLogo from "@/assets/images/home/webtricker-w.png";
+
+// dynamic metadata for services page
+export async function generateMetadata() {
+  const data = await getServicesData();
+  return {
+    title: "Our Services | Webtricker",
+    description: "Explore the range of services we offer at Webtricker.",
+    keywords: [
+      data.map((service: { title: string }) => service.title).join(", "),
+    ],
+    openGraph: {
+      title: "Webtricker - Our Services",
+      description: "Explore the range of services we offer at Webtricker.",
+      images: [
+        {
+          url: `${shortLogo.src}`,
+          width: 1200,
+          height: 630,
+          alt: "Webtricker Services Banner",
+        },
+      ],
+    },
+  };
+}
 
 export default async function ServicesPage() {
   const serviceData = await getServicesData();
+  console.log(serviceData);
   return (
     <main className="w-full z-0">
       <ServicesBanner />
