5a54cc66f0e3dc8f10e7e4ea5be2b66ba7be1679
dev-ahad-ali <ahadali.dev@gmail.com>
2025-09-05 18:46:28 +0600

    Update: google map iframe link dynamicly added

--- src/app/(public pages)/contact/page.tsx
+++ src/app/(public pages)/contact/page.tsx
@@ -3,7 +3,6 @@ import ContactForm from "./components/ContactForm";
 import Container from "@/sharedComponets/ui/wrapper/Container";
 import Image from "next/image";
 import ContactCTABtns from "./components/ContactCTABtns";
-import { EmailIcon, PhoneIcon } from "./components/Icons";
 import Link from "next/link";
 import { Metadata } from "next";
 import shortLogo from "@/assets/images/home/webtricker-w.png";
@@ -127,103 +126,70 @@ export default async function ContactPage() {
                   <h6 className="heading mb-1">
                     {contactPageData?.address?.title}
                   </h6>
-                  <address className="not-italic">
-                    <strong>US Office</strong>: Hurst, Texas, United States
-                  </address>
-                  <address className="not-italic">
-                    <strong>Dhaka Office</strong>: KHL Laboni&apos;s Dream,
-                    Plot-06, Afroza Begum Rd, Dhaka 1229
-                  </address>
-                  <address className="not-italic">
-                    <strong>Jamalpur Office</strong>: House No-46, Zia College
-                    Moar, Beside Sohid Minar, Jamalpur, Bangladesh.
-                  </address>
+                  {contactPageData?.address?.addresses?.map((addr) => (
+                    <address className="not-italic" key={addr?.location}>
+                      <strong>{addr?.office}</strong>: {addr?.location}
+                    </address>
+                  ))}
                 </div>
               </div>
               <div className="w-full flex gap-5 items-start">
-                <PhoneIcon className="min-w-5 w-6 mt-1" />
+                <Image
+                  src={contactPageData?.contactNumber?.iconBlack}
+                  alt={contactPageData?.contactNumber?.title}
+                  width={24}
+                  height={24}
+                  className="min-w-5 w-6 mt-1"
+                />
                 <div className="grow">
-                  <h6 className="heading mb-1">Phone</h6>
-                  <a
-                    href="tel:+8809639237100"
-                    className="block animate-underline w-fit"
-                  >
-                    +8809639237100
-                  </a>
-                  <a
-                    href="tel:+8809639237101"
-                    className="block animate-underline w-fit"
-                  >
-                    +8809639237101
-                  </a>
-                  <a
-                    href="tel:+16824726184"
-                    className="block animate-underline w-fit"
-                  >
-                    +1 (682) 472-6184
-                  </a>
-                  <a
-                    href="tel:+8801712377577"
-                    className="block animate-underline w-fit"
-                  >
-                    +8801712377577
-                  </a>
-                  <a
-                    href="tel:+8801785696469:"
-                    className="block animate-underline w-fit"
-                  >
-                    +8801785696469
-                  </a>
-                  <a
-                    href="tel:+8801793544335"
-                    className="block animate-underline w-fit"
-                  >
-                    +8801793544335
-                  </a>
+                  <h6 className="heading mb-1">
+                    {contactPageData?.contactNumber?.title}
+                  </h6>
+                  {contactPageData?.contactNumber?.numbers?.map((num) => (
+                    <a
+                      key={num}
+                      href={`tel:${num}`}
+                      className="block animate-underline w-fit"
+                    >
+                      {num}
+                    </a>
+                  ))}
                 </div>
               </div>
               <div className="w-full flex gap-5 items-start">
-                <EmailIcon className="min-w-5 w-6 mt-1" />
+                <Image
+                  src={contactPageData?.contactMails?.iconBlack}
+                  alt={contactPageData?.contactMails?.title}
+                  width={24}
+                  height={24}
+                  className="min-w-5 w-6 mt-1"
+                />
                 <div className="grow flex flex-col">
-                  <h6 className="heading mb-1">Email</h6>
-                  <Link
-                    title="Email"
-                    href="mailto:info@webtricker.com"
-                    className="animate-underline w-fit"
-                  >
-                    info@webtricker.com
-                  </Link>
-                  <Link
-                    title="Email"
-                    href="mailto:inquiry@webtricker.com"
-                    className="animate-underline w-fit "
-                  >
-                    inquiry@webtricker.com
-                  </Link>
-                  <Link
-                    title="Email"
-                    href="mailto:career@webtricker.com"
-                    className="animate-underline w-fit"
-                  >
-                    career@webtricker.com
-                  </Link>
-                  <Link
-                    title="Email"
-                    href="mailto:admin@webtricker.com"
-                    className="animate-underline w-fit"
-                  >
-                    admin@webtricker.com
-                  </Link>
+                  <h6 className="heading mb-1">
+                    {contactPageData?.contactMails?.title}
+                  </h6>
+                  {contactPageData?.contactMails?.mails?.map((mail) => (
+                    <Link
+                      key={mail}
+                      title="Email"
+                      href={`mailto:${mail}`}
+                      className="animate-underline w-fit"
+                    >
+                      {mail}
+                    </Link>
+                  ))}
                 </div>
               </div>
             </div>
 
             {/* ======= map ======== */}
             <div className="w-full overflow-hidden rounded-[10px]">
-              <h6 className="heading mb-5">Find us on google map:</h6>
+              <h6 className="heading mb-5">
+                {contactPageData?.googleMap?.title}
+              </h6>
               <iframe
                 className="min-h-[400px]"
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14474.096950431665!2d89.941474!3d24.914205!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fd7f5ce07e179d%3A0x209802aa6366f9da!2sWebtricker%20Web%20Design%20%26%20Development%20Agency!5e0!3m2!1sen!2sus!4v1693518943068!5m2!1sen!2sus"
+                src={contactPageData?.googleMap?.iframe}
                 width="100%"
                 height="100%"
                 loading="lazy"
