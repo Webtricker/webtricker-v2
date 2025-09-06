53cea224ef8117021d6a9f93a9130a892b1ea382
dev-ahad-ali <ahadali.dev@gmail.com>
2025-09-06 17:25:51 +0600

    spacing changed

--- src/app/(private pages)/settings/home/page.tsx
+++ src/app/(private pages)/settings/home/page.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import HomePageForm from "./components/HomePageForm";
-import { getPostsData, getServicesData, getTestimonialsData } from "@/utils/pageData";
+import {
+  getPostsData,
+  getServicesData,
+  getTestimonialsData,
+} from "@/utils/pageData";
 
 export default async function HomeCustomizationPage() {
   const testimonialsData = await getTestimonialsData();
-   const serviceData = await getServicesData(4);
-   const posts = await getPostsData(4);
+  const serviceData = await getServicesData(4);
+  const posts = await getPostsData(4);
   return (
     <>
-      <HomePageForm posts={posts} serviceData={serviceData} testimonials={testimonialsData} />
+      <HomePageForm
+        posts={posts}
+        serviceData={serviceData}
+        testimonials={testimonialsData}
+      />
     </>
   );
 }
