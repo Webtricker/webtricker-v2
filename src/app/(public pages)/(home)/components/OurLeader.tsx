f18e860169ed6cae12c67a0664e25c1a06015613
dev-ahad-ali <ahadali.dev@gmail.com>
2025-09-10 11:28:58 +0600

    Fix: leaders section responsive fixed

--- src/app/(public pages)/(home)/components/OurLeader.tsx
+++ src/app/(public pages)/(home)/components/OurLeader.tsx
@@ -3,19 +3,25 @@ import Container from "@/sharedComponets/ui/wrapper/Container";
 import { ILeaderInfo } from "@/types/data";
 import React from "react";
 
-export default function OurLeader({title,leaderData=[]}:{title:string,leaderData:ILeaderInfo[]}) {
+export default function OurLeader({
+  title,
+  leaderData = [],
+}: {
+  title: string;
+  leaderData: ILeaderInfo[];
+}) {
   return (
     <section className="py-8 md:py-10 lg:py-14 xl:py-16 2xl:py-18">
-        <div className="w-full text-center mb-14 md:mb-16 lg:mb-20">
-            <h3 className="text-center middle-border">{title || "Our Leaders"}</h3>
-        </div>
-      <div className="w-full overflow-x-auto">
-        <Container className="py-2 min-w-[620px] team-members-wrap flex justify-center w-full gap-5 lg:gap-10">
-        {leaderData.map((team) => (
-          <TeammemberCard team={team} key={team.name} />
-        ))}
-      </Container>
+      <div className="w-full text-center mb-14 md:mb-16 lg:mb-20">
+        <h3 className="text-center middle-border">{title || "Our Leaders"}</h3>
       </div>
-    </section>  
-  ); 
+      <div className="w-full overflow-x-auto">
+        <Container className="py-2 team-members-wrap flex flex-wrap justify-center w-full gap-5 lg:gap-10 items-center">
+          {leaderData.map((team) => (
+            <TeammemberCard team={team} key={team.name} />
+          ))}
+        </Container>
+      </div>
+    </section>
+  );
 }
