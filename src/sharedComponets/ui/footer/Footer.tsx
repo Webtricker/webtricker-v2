12aa9642b78c50b30d1aadc89307197b1657485c
dev-ahad-ali <ahadali.dev@gmail.com>
2025-09-13 12:17:50 +0600

    Update: footer added as v1

--- src/sharedComponets/ui/footer/Footer.tsx
+++ src/sharedComponets/ui/footer/Footer.tsx
@@ -3,11 +3,17 @@ import Container from "../wrapper/Container";
 import Link from "next/link";
 import NewsLetterForm from "./NewsLetterForm";
 import BouncingText from "../effects/BouncingText";
-import { getPublicFooterData, getServicesData } from "@/utils/pageData";
+import {
+  getContactPageData,
+  getPublicFooterData,
+  getServicesData,
+} from "@/utils/pageData";
 import { footerServicesLink, TFooterService } from "@/data/pageData";
 import { IService } from "@/types/post";
 import Image from "next/image";
 import { IFooter } from "@/types/componentsType";
+import { IContactPage } from "@/types/pageTypes";
+import footerBg from "@/assets/images/footer/footer-bg.svg";
 
 export default async function Footer() {
   const services = (await getServicesData(4)) as IService[];
@@ -19,21 +25,25 @@ export default async function Footer() {
 
   // footer data
   const footerData = (await getPublicFooterData()) as IFooter;
+  const contactPageData = (await getContactPageData()) as IContactPage;
+
   return (
-    <footer className="mt-8 md:mt-10 lg:mt-14 xl:mt-16 2xl:mt-18">
-      <div className="w-full bg-slate-200 dark:bg-black py-8 md:py-10 lg:py-14 xl:py-16 2xl:py-18">
+    <footer className="mt-8 md:mt-10 lg:mt-14 xl:mt-16 2xl:mt-18 relative">
+      <div className="absolute inset-0 w-full h-full z-10">
+        <Image
+          src={footerBg?.src}
+          width={1920}
+          height={300}
+          alt="Footer background"
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="w-full bg-[#141623] py-8 md:py-10 lg:py-14 xl:py-16 2xl:py-18 text-slate-200">
         <Container className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 md:gap-6 lg:gap-8 2xl:gap-16">
           <div className="w-full md:mt-[-4px]">
             <Link className="flex items-center gap-1 bold" href="/">
               <Image
-                className="inline dark:hidden w-[160px] md:w-[180px] lg:w-[190px] xl:w-[200px] h-auto"
-                src={footerData?.logo?.black || ""}
-                width={282}
-                height={74}
-                alt="Site logo"
-              />
-              <Image
-                className="hidden dark:inline w-[160px] md:w-[180px] lg:w-[190px] xl:w-[200px] h-auto"
+                className="inline w-[160px] md:w-[180px] lg:w-[190px] xl:w-[200px] h-auto"
                 src={footerData?.logo?.white || ""}
                 width={282}
                 height={74}
@@ -43,7 +53,7 @@ export default async function Footer() {
             <p className="mt-3 wt_fs-md">{footerData?.description || ""}</p>
           </div>
           <div className="ms-0 md:ms-6 w-full">
-            <h5>{footerData?.pages?.title || ""}</h5>
+            <h5 className="font-semibold">{footerData?.pages?.title || ""}</h5>
             <div className="mt-3 items-start flex flex-col gap-2 w-full wt_fs-md">
               {footerData?.pages?.links &&
               (footerData?.pages?.links as IFooter["pages"]["links"]).length >
@@ -66,7 +76,9 @@ export default async function Footer() {
             </div>
           </div>
           <div className="w-full">
-            <h5>{footerData?.services?.title || ""}</h5>
+            <h5 className="font-semibold">
+              {footerData?.services?.title || ""}
+            </h5>
             <div className="mt-3 items-start flex flex-col gap-2 w-full wt_fs-md">
               {footerData?.services?.links &&
               (footerData?.services?.links as IFooter["services"]["links"])
@@ -95,7 +107,9 @@ export default async function Footer() {
             </div>
           </div>
           <div className="w-full">
-            <h5>{footerData?.socialLinks?.title || ""}</h5>
+            <h5 className="font-semibold">
+              {footerData?.socialLinks?.title || ""}
+            </h5>
             <div className="flex gap-5 md:gap-7 w-full mt-3">
               {footerData?.socialLinks?.links &&
               (
@@ -130,10 +144,105 @@ export default async function Footer() {
             </div>
           </div>
         </Container>
+        <Container className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 md:gap-6 lg:gap-8 2xl:gap-16 mt-6 md:mt-10">
+          <div className="w-full flex gap-5 items-start">
+            <Image
+              src={contactPageData?.address?.iconWhite}
+              alt={contactPageData?.address?.title}
+              width={24}
+              height={24}
+              className="min-w-5 w-6"
+            />
+            <div className="grow not-italic">
+              <h6 className="heading mb-1 !text-slate-200 font-semibold">
+                {contactPageData?.address?.title}
+              </h6>
+              {contactPageData?.address?.addresses?.map((addr) => (
+                <address className="not-italic" key={addr?.location}>
+                  <strong>{addr?.office}</strong>: {addr?.location}
+                </address>
+              ))}
+            </div>
+          </div>
+          <div className="w-full flex gap-5 items-start">
+            <Image
+              src={contactPageData?.contactNumber?.iconWhite}
+              alt={contactPageData?.contactNumber?.title}
+              width={24}
+              height={24}
+              className="min-w-5 w-6 mt-1"
+            />
+            <div className="grow">
+              <h6 className="heading mb-1 !text-slate-200 font-semibold">
+                {contactPageData?.contactNumber?.title}
+              </h6>
+              {contactPageData?.contactNumber?.numbers
+                ?.slice(0, 3)
+                ?.map((num) => (
+                  <a
+                    key={num}
+                    href={`tel:${num}`}
+                    className="block animate-underline w-fit"
+                  >
+                    {num}
+                  </a>
+                ))}
+            </div>
+          </div>
+          <div className="w-full flex gap-5 items-start">
+            <Image
+              src={contactPageData?.contactNumber?.iconWhite}
+              alt={contactPageData?.contactNumber?.title}
+              width={24}
+              height={24}
+              className="min-w-5 w-6 mt-1"
+            />
+            <div className="grow">
+              <h6 className="heading mb-1 !text-slate-200 font-semibold">
+                Hotline
+              </h6>
+              {contactPageData?.contactNumber?.numbers
+                ?.slice(3, 6)
+                ?.map((num) => (
+                  <a
+                    key={num}
+                    href={`tel:${num}`}
+                    className="block animate-underline w-fit"
+                  >
+                    {num}
+                  </a>
+                ))}
+            </div>
+          </div>
+          <div className="w-full flex gap-5 items-start">
+            <Image
+              src={contactPageData?.contactMails?.iconWhite}
+              alt={contactPageData?.contactMails?.title}
+              width={24}
+              height={24}
+              className="min-w-5 w-6 mt-1"
+            />
+            <div className="grow flex flex-col">
+              <h6 className="heading mb-1 !text-slate-200 font-semibold">
+                {contactPageData?.contactMails?.title}
+              </h6>
+              {contactPageData?.contactMails?.mails?.map((mail) => (
+                <Link
+                  key={mail}
+                  title="Email"
+                  href={`mailto:${mail}`}
+                  className="animate-underline w-fit"
+                >
+                  {mail}
+                </Link>
+              ))}
+            </div>
+          </div>
+        </Container>
         <Container className="my-10">
           <div className="w-full flex items-center justify-center">
             <BouncingText
-              size="wt_fs-7xl"
+              size="wt_fs-7xl text-slate-200"
               text={footerData?.bounchingTxt || ""}
               interval={100}
               duration={4}
