c8751a39edbc09ed4f33e18e2184fff8e25efbf3
dev-ahad-ali <ahadali.dev@gmail.com>
2025-09-20 19:45:32 +0600

    Fix: terms and conditions page style changed

--- src/app/(public pages)/privacy-policy/page.tsx
+++ src/app/(public pages)/privacy-policy/page.tsx
@@ -6,6 +6,8 @@ import { getPrivacyPolicyPageData } from "@/utils/pageData";
 import ConditionalReturnContainer from "@/sharedComponets/ui/wrapper/ConditionalReturnContainer";
 import { Metadata } from "next";
 import Container from "@/sharedComponets/ui/wrapper/Container";
+import Image from "next/image";
+import bannerImg from "@/assets/images/policy-terms/privacy-policy-bg.webp";
 
 export const revalidate = 120;
 
@@ -27,16 +29,36 @@ export default async function PrivacyPolicyPage() {
     );
   }
   return (
-    <main className="max-w-[900px] mx-auto px-4 z-0 section-speacing mt-40 post-details-container">
-      <Container className="w-full">
-        <h1 className="wt_fs-4xl my-20">{data.title}</h1>
-        <p className="my-3">{data.description}</p>
-      </Container>
-      <section className="w-full wt_parser_content mt-20">
-        <BlogPageContainer>
-          <HtmlContentParser htmlContent={data?.content} />
-        </BlogPageContainer>
+    <main>
+      <section
+        className={`w-full min-h-screen z-0 flex relative mb-8 md:mb-10 lg:mb-14 xl:mb-16 2xl:mb-18`}
+      >
+        <Container className="flex items-center justify-center">
+          <div className="w-full max-w-[1000px] text-center bg-slate-800/30 rounded-[10px] p-4">
+            <h1 className="!text-white wt_text-shadow wt_fs-7xl font-medium heading !leading-[100%]">
+              {data?.title}
+            </h1>
+          </div>
+        </Container>
+        <Image
+          width={1800}
+          height={900}
+          src={bannerImg?.src}
+          className=" absolute top-0 left-0 w-full h-full object-cover -z-10"
+          alt="Privacy policy Banner Image"
+        />
       </section>
+      <div className="max-w-[900px] mx-auto px-4">
+        <Container className="w-full">
+          <h1 className="wt_fs-4xl my-20">{data.title}</h1>
+          <p className="my-3">{data.description}</p>
+        </Container>
+        <section className="w-full wt_parser_content mt-20">
+          <BlogPageContainer>
+            <HtmlContentParser htmlContent={data?.content} />
+          </BlogPageContainer>
+        </section>
+      </div>
     </main>
   );
 }
--- src/app/(public pages)/terms-and-conditions/page.tsx
+++ src/app/(public pages)/terms-and-conditions/page.tsx
@@ -6,6 +6,8 @@ import { getTermsAndConditionPageData } from "@/utils/pageData";
 import ConditionalReturnContainer from "@/sharedComponets/ui/wrapper/ConditionalReturnContainer";
 import { Metadata } from "next";
 import Container from "@/sharedComponets/ui/wrapper/Container";
+import bannerImg from "@/assets/images/policy-terms/terms-and-contions.webp";
+import Image from "next/image";
 
 export const revalidate = 120;
 
@@ -27,16 +29,36 @@ export default async function TermsAndConditionPage() {
     );
   }
   return (
-    <main className="max-w-[900px] px-4 mx-auto z-0 section-speacing mt-40 post-details-container">
-      <Container className="w-full">
-        <h1 className="wt_fs-4xl my-20">{data.title}</h1>
-        <p className="my-3">{data.description}</p>
-      </Container>
-      <section className="w-full wt_parser_content mt-20">
-        <BlogPageContainer>
-          <HtmlContentParser htmlContent={data?.content} />
-        </BlogPageContainer>
+    <main>
+      <section
+        className={`w-full min-h-screen z-0 flex relative mb-8 md:mb-10 lg:mb-14 xl:mb-16 2xl:mb-18`}
+      >
+        <Container className="flex items-center justify-center">
+          <div className="w-full max-w-[1000px] text-center bg-slate-800/30 rounded-[10px] p-4">
+            <h1 className="!text-white wt_text-shadow wt_fs-7xl font-medium heading !leading-[100%]">
+              {data?.title}
+            </h1>
+          </div>
+        </Container>
+        <Image
+          width={1800}
+          height={900}
+          src={bannerImg?.src}
+          className=" absolute top-0 left-0 w-full h-full object-cover -z-10"
+          alt="Terms and conditions banner image"
+        />
       </section>
+      <div className="max-w-[900px] mx-auto px-4">
+        <Container className="w-full">
+          <h1 className="wt_fs-4xl my-20">{data.title}</h1>
+          <p className="my-3">{data.description}</p>
+        </Container>
+        <section className="w-full wt_parser_content mt-20">
+          <BlogPageContainer>
+            <HtmlContentParser htmlContent={data?.content} />
+          </BlogPageContainer>
+        </section>
+      </div>
     </main>
   );
 }
